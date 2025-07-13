c7fccfb605021cb648338d80447dbc2322b443ae
Alex-Mawunya-Tagbor <alexm8761@gmail.com>
2025-07-13 06:55:06 +0000

    fic factorial recursively bug

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,8 +1,14 @@
-function add(a, b) {
-  return a + b;
+function factorial(n) {
+    n = parseInt(n);
+    if (isNaN(n)) {
+    return 1;
+    }
+    if (n <= 1) {
+    return 1;
+    }
+    return n * factorial(n - 1);
 }
 
-const a = Number(process.argv[2]);
-const b = Number(process.argv[3]);
-
-console.log(add(a, b));
+const input = process.argv[2];
+const result = factorial(input);
+console.log(`Factorial of ${input} is: ${result}`);

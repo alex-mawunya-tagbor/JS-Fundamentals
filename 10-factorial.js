9f5ea82a5194f56b69bfb9792eff1a58d6a84152
Alex-Mawunya-Tagbor <alexm8761@gmail.com>
2025-07-13 07:02:45 +0000

    fix factorial bug 2

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,14 +1,14 @@
 function factorial(n) {
-    n = parseInt(n);
-    if (isNaN(n)) {
+  n = parseInt(n);
+  if (isNaN(n)) {
     return 1;
-    }
-    if (n <= 1) {
+  }
+  if (n <= 1) {
     return 1;
-    }
-    return n * factorial(n - 1);
+  }
+  return n * factorial(n - 1);
 }
 
 const input = process.argv[2];
 const result = factorial(input);
-console.log(`Factorial of ${input} is: ${result}`);
+console.log(result);
